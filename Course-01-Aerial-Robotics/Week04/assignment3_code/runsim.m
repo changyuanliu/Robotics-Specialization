05d47aba73fe818f4f0c006bc871312940927ede
ChangYuan Liiu <changyuan@ustc.edu>
2021-04-23 23:13:57 -0400

    Update runsim.m

--- Course-01-Aerial-Robotics/Week04/assignment3_code/runsim.m
+++ Course-01-Aerial-Robotics/Week04/assignment3_code/runsim.m
@@ -1,9 +1,7 @@
 close all;
 clear;
 
-
 addpath('utils');
-
 
 %% pre-calculated trajectories
 trajhandle = @traj_line;
@@ -21,7 +19,6 @@ trajhandle = @traj_line;
 
 %% controller
 controlhandle = @controller;
-
 
 % Run simulation with given trajectory generator and controller
 % state - n x 13, with each row having format [x, y, z, xdot, ydot, zdot, qw, qx, qy, qz, p, q, r]
